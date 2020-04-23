ce0f0fc0692f19d524690d510b0675646724f078
Your Name <you@example.com>
2020-04-23 16:56:23 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+window.onload=function(){
+	let calculator = document.querySelector( ' .calculator ' )
+let keys = calculator.querySelector( ' .calculator__keys ' )
+keys.addEventListener( ' click ' , function(e) {
+if (e.target.matches( ' button ' )) {
+// Do something
+}
+})
+let key = e.target
+let action = key.dataset.action
+
+if (!action) {
+console.log( ' number key! ' )
+}
+
+if (action === ' add ' ||
+action === ' subtract ' ||
+action === ' multiply ' ||
+action === ' divide ' ) { console.log( ' operator key! ' ) }
+
+if (action === ' decimal ' ) {
+console.log( ' decimal key! ' )
+}
+if (action === ' clear ' ) {
+console.log( ' clear key! ' )
+}
+if (action === ' calculate ' ) {
+console.log( ' equal key! ' )
+}
+
+const display = document.querySelector( ' .calculator__display ' )
+keys.addEventListener( ' click ' , e => {
+if (e.target.matches( ' button ' )) {
+const key = e.target
+const action = key.dataset.action
+const keyContent = key.textContent
+const displayedNum = display.textContent
+// ...
+}
+})
+if (!action) {
+if (displayedNum === ' 0 ' ) {
+display.textContent = keyContent
+}
+}
+
+if (!action) {
+if (displayedNum === ' 0 ' ) {
+display.textContent = keyContent
+} else {
+display.textContent = displayedNum + keyContent
+}
+}
+
+
+
+}
